696168227d4911c29c17f2510af17fc562f40ae0
Erlend Tøssebro <erlend.tossebro@uis.no>
2025-09-23 08:34:28 +0200

    Flyttet til funksjon
    Flyttet koden til funksjon og gjorde slik at den kan brukes flere ganger

--- turtle_eksempel.py
+++ turtle_eksempel.py
@@ -1,12 +1,21 @@
 import turtle
 
 # Tegner en firkant med Turtle Graphics
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.done()
+def tegn_firkant(sidelengde, x_start=0, y_start=0):
+    turtle.penup()
+    turtle.goto(x_start, y_start)
+    turtle.pendown()
+    turtle.forward(sidelengde)
+    turtle.right(90)
+    turtle.forward(sidelengde)
+    turtle.right(90)
+    turtle.forward(sidelengde)
+    turtle.right(90)
+    turtle.forward(sidelengde)
+    turtle.right(90)
+
+
+if __name__ == "__main__":
+    tegn_firkant(100)
+    tegn_firkant(50, -100, 100)
+    turtle.done()
